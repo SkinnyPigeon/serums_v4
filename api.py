639a34a23ce9957df622ee3eba256e69a05d33a2
skinnypigeon <euan.blackledge@soprasteria.com>
2021-05-28 11:26:58 +0100

    got the jwt verification working

--- api.py
+++ api.py
@@ -9,6 +9,11 @@ import subprocess
 import json
 
 
+# Functions
+
+from functions.jwt import validate_jwt
+
+
 # Setting up environment
 
 FLASK_DEBUG=1
@@ -39,11 +44,15 @@ parser = api.parser()
 # parser.add_argument('Name of field', type=list, required=True, help='Field used as part of request or reply', location='json')
 # parser.add_argument('Name of another field', type=list, required=True, help='Another field used as part of request or reply', location='json')
 
+jwt_parser = api.parser()
+jwt_parser.add_argument('Authorization', help="The authorization token", location="headers", default="""Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYyMDkwNTkzOCwianRpIjoiNjZjNTgwYmUtOTViMC00YjhiLWE3ZjQtYzU3ODkyOGJhM2NjIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRlc3QiLCJuYmYiOjE2MjA5MDU5MzgsImV4cCI6MTg4MDEwNTkzOH0.zeJNNiXE7XbeNPC5g2OEQvu1EsYeohUsgvsY2_fg8EM""")
 
 
 # Name spaces
 
 hello_space = api.namespace('hello', description='Check the server is on')
+staff_space = api.namespace('staff tables', description='Return the staff tables')
+jwt_space = api.namespace('jwt', description='JWT endpoints')
 
 
 # Routes
@@ -53,6 +62,24 @@ class ServerCheck(Resource):
     @api.marshal_with(hello)
     def get(self):
         return {"hello": "Welcome to the API. The server is on"}
+
+@staff_space.route('/department')
+class Department(Resource):
+    def post(self):
+
+
+        return 200
+
+@jwt_space.route("/authorize_jwt", methods=["POST"])
+class AuthJWT(Resource):
+    '''Verify JWT with Authentication module'''
+    def post(self):
+
+        jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjIyODAwNTI4LCJqdGkiOiJmZDk1MzcxOTVlOTE0ZTk1YjllZWI4MjMwYmViODVlOSIsInVzZXJJRCI6MzY0LCJpc3MiOiJTZXJ1bXNBdXRoZW50aWNhdGlvbiIsImlhdCI6MTYyMjE5NTcyOCwic3ViIjoiZXVhbkB0ZXN0LmNvbSIsImdyb3VwSURzIjpbIlBBVElFTlQiXSwib3JnSUQiOiJVU1RBTiIsImF1ZCI6Imh0dHBzOi8vdXJsZGVmZW5zZS5wcm9vZnBvaW50LmNvbS92Mi91cmw_dT1odHRwLTNBX193d3cuc2VydW1zLmNvbSZkPUR3SURhUSZjPWVJR2pzSVRmWFBfeS1ETExYMHVFSFhKdlU4bk9IclVLOElyd05LT3RrVlUmcj11VGZONXVRMWtod2JSeV9UZ0tINmFVZDAtQmJtMEc4Sy1WYWprelpteTk4Jm09MmlVTm4yOUZTYWY3LTAzeHU5eE1CcmNuNHQ2VV8zdzN1cUxpTHl0VGZUNCZzPTVqQjJqbXFoc05BX2cxU1Z5WmdVRlJGOW9FUDhfQVFhLWxpY1lXM0l1ZncmZT0ifQ.7xY76j-_K7r3Dos7E0aoUoCnMmGLXwwzDa-d1TYRkeQ".encode('ascii', 'ignore').decode('unicode_escape')
+        # print(jwt.encode('latin-1'))
+        validate_jwt(jwt)
+        return 200
+
 
 
 if __name__ == '__main__':
--- functions/jwt.py
+++ functions/jwt.py
@@ -1,33 +1,36 @@
 import requests
 
-jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjIxODY4MjgyLCJqdGkiOiI0NTMzYmU3Mjg0YWQ0MTVkOGRmMGM0ZTIyNzJhOTY2YSIsInVzZXJJRCI6MzYzLCJpc3MiOiJTZXJ1bXNBdXRoZW50aWNhdGlvbiIsImlhdCI6MTYyMTI2MzQ4Miwic3ViIjoiYXJnQGFjYy5jb20iLCJncm91cElEcyI6WyJQQVRJRU5UIl0sIm9yZ0lEIjoiVVNUQU4iLCJhdWQiOiJodHRwczovL3VybGRlZmVuc2UucHJvb2Zwb2ludC5jb20vdjIvdXJsP3U9aHR0cC0zQV9fd3d3LnNlcnVtcy5jb20mZD1Ed0lEYVEmYz1lSUdqc0lUZlhQX3ktRExMWDB1RUhYSnZVOG5PSHJVSzhJcndOS090a1ZVJnI9dVRmTjV1UTFraHdiUnlfVGdLSDZhVWQwLUJibTBHOEstVmFqa3pabXk5OCZtPTJpVU5uMjlGU2FmNy0wM3h1OXhNQnJjbjR0NlVfM3czdXFMaUx5dFRmVDQmcz01akIyam1xaHNOQV9nMVNWeVpnVUZSRjlvRVA4X0FRYS1saWNZVzNJdWZ3JmU9In0.OpQsi5UmFVz_IHHI53bGTjhENJ3Pnht1jm9ZV4MMG8k"
 
 def validate_jwt(jwt):
     url = "https://authentication.serums.cs.st-andrews.ac.uk/ua/verify_jwt/"
+    # url = "https://localhost:9000/ua/verify_jwt/"
     '''
     You might as well use the following url since apps can see each other on Fracas:
     url = "https://localhost:9000/ua/verify_jwt/"
     '''
-    payload = "{​}​"
-    headers = {​
-    'Authorization': 'Bearer {​}​'.format(jwt),
-    'Content-Type': 'application/json'
-    }​
+    payload = ""
+
+    headers = {
+        "Authorization": "Bearer {}".format(jwt),
+        "Content-Type": "application/json"
+    }
+
     try:
         response = requests.request("POST", url, headers=headers, data=payload)
+        print(response)
         if response.status_code == 200:
             print(
-                "SUCCESS. Status code: {​}​ - Result: {​}​".format(
+                "SUCCESS. Status code: {} - Result: {}".format(
                     response.status_code,
                     response.json()
                 )
             )
         else:
             print(
-                "ERROR. Status code: {​}​ - Reason: {​}​".format(
+                "ERROR. Status code: {}​ - Reason: {}​".format(
                     response.status_code,
                     response.text
                 )
             )
     except Exception as e:
-        print("Failed to make request. Reason: {​}​".format(str(e)))
+        print("Failed to make request. Reason: {}​".format(str(e)))
