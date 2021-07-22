d4b41172f6fccb3af9527f9d1da115951d995af5
skinnypigeon <euan.blackledge@soprasteria.com>
2021-07-22 13:25:56 +0100

    added the other dockerfiles in so it's ready

---
+++ 05-fill-source-tables.py
@@ -0,0 +1,29 @@
+# Setup
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/code/api_v3_docker/code/api')
+load_dotenv(os.path.join(project_folder, '.env'))
+PORT = os.getenv('PGPORT')
+PASSWORD = os.getenv('PGPASSWORD')
+if PORT == None:
+    PASSWORD = os.environ.get('PGPASSWORD')
+    PORT = os.environ.get('PGPORT')
+
+engine = create_engine("postgresql://postgres:{}@localhost:{}/source".format(PASSWORD, PORT))
+
+# Creating source tables
+
+tables = ['fcrb_data', 'ustan_data', 'zmc_data']
+
+try:
+    for table in tables:
+        sql_file = open('./code/api/reset_sql/sql/{table}.sql'.format(table=table), 'r')
+        sql = text(sql_file.read())
+        engine.execute(sql)
+except:
+    for table in tables:
+        sql_file = open('/code/api/reset_sql/sql/{table}.sql'.format(table=table), 'r')
+        sql = text(sql_file.read())
+        engine.execute(sql)
