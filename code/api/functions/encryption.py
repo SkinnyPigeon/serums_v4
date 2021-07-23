303d7d759754ae0bac18b07127cc0f107e1759d7
skinnypigeon <euan.blackledge@soprasteria.com>
2021-07-23 13:43:00 +0100

    that's the encryption and decryption working

--- code/api/functions/encryption.py
+++ code/api/functions/encryption.py
@@ -1,5 +1,35 @@
 from cryptography.fernet import Fernet
 
+import os
+import json
 import base64
 import binascii
+from datetime import datetime
 
+secret = os.environ.get('BCPASSWORD')
+
+data = {
+    'type': 'data_vault_created',
+    'hospital_id': 'zmc',
+    'status': 'success',
+    'content': {
+        'schema': '_abc92039'
+    },
+    'date': str(datetime.today())
+}
+
+def encrypt_data(data):
+    encryption = Fernet(secret)
+    encrypted_data = encryption.encrypt(json.dumps(data).encode())
+    return encrypted_data.decode()
+
+encrypted_data = encrypt_data(data)
+print(encrypted_data)
+
+def decrypt_data(data):
+    decryption = Fernet(secret)
+    decrypted_data = decryption.decrypt(data.encode())
+    return decrypted_data.decode()
+
+decrypted_data = decrypt_data(encrypted_data)
+print(decrypted_data)
