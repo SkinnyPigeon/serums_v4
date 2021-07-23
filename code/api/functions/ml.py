4851287759653cda475cd22407abab6e3e8fb57e
skinnypigeon <euan.blackledge@soprasteria.com>
2021-07-23 10:47:15 +0100

    raw results can be shared for ml module for single patient based on sign in

--- code/api/functions/ml.py
+++ code/api/functions/ml.py
@@ -10,6 +10,9 @@ from dotenv import load_dotenv
 from pathlib import Path
 import subprocess
 
+import json
+import pandas as pd
+
 project_folder = subprocess.check_output("pwd", shell=True).decode("utf-8").rstrip()
 load_dotenv(os.path.join(project_folder, '.env'))
 PORT = os.getenv('PGPORT')
@@ -45,6 +48,25 @@ def object_as_dict(obj):
     return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
 
 
+def convert_dates_to_string(df):
+    for column in df:
+        print(df[column].dtypes)
+        if df.dtypes[column] in ['datetime64[ns]']:
+            df[column] = df[column].astype(str)
+    return df
+
+
+def convert_decimal_to_float(df):
+    for column in df:
+        if df.dtypes[column] in ['object']:
+            try:
+                df[column] = df[column].astype(float)
+            except:
+                pass
+    return df
+
+
+
 # Converting Serums ID into source system ID    
 
 
@@ -68,36 +90,39 @@ def select_source_patient_id_value(session, id_class, serums_id, key_name):
 
 def select_patient_data(session, table_class, patient_id, key_name):
     results = session.query(table_class).filter_by(**{key_name: patient_id}).all()
-    data = []
-    for result in results:
-        print(dir(result))
-        # data.append(result)
-    return data 
+    data = []    
+    for row in results:
+        data.append(object_as_dict(row))
+
+    df = pd.DataFrame([x for x in data]) 
+    df = convert_dates_to_string(df)
+    df = convert_decimal_to_float(df)
+    return df.to_dict('index')
+
+
+
+
 
 
 def get_patient_data(body):
     tables = ['cycles', 'general', 'intentions', 'patients', 'regimes']
-    connection = setup_connection(body)
-    classes = select_table_classes(connection['schema'], connection['base'])
-    print("CLASSES: {}".format(dir(connection['base'].classes)))
-    id_class = connection['base'].classes.serums_ids
-    key_name = select_source_patient_id_name(body)
-    patient_id = select_source_patient_id_value(connection['session'], 
-                                                id_class, 
-                                                body['userID'], key_name)
-    print("PATIENT ID: {}".format(patient_id))
-
-    results = {}
-    for table in tables:
-        print(table)
-        table_class = connection['base'].classes[table]
-        data = select_patient_data(connection['session'], table_class, patient_id, key_name)
-        results[table] = data
-    
-    connection['engine'].dispose()
-    
-    return results
-
-    # except Exception as e:
-    #     connection['engine'].dispose()
-    #     return {"error": str(e)}
+    try:
+        connection = setup_connection(body)
+        classes = select_table_classes(connection['schema'], connection['base'])
+        id_class = connection['base'].classes.serums_ids
+        key_name = select_source_patient_id_name(body)
+        patient_id = select_source_patient_id_value(connection['session'], 
+                                                    id_class, 
+                                                    body['userID'], key_name)
+        results = {}
+        for table in tables:
+            print(table)
+            table_class = connection['base'].classes[table]
+            data = select_patient_data(connection['session'], table_class, patient_id, key_name)
+            results[table] = data
+        print(json.dumps(results, indent=2))
+        connection['engine'].dispose()
+        return results
+    except Exception as e:
+        connection['engine'].dispose()
+        return {"error": str(e)}
