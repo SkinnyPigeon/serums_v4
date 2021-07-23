9f79c3b9b017c14c1c2e9c6ec2d2787add37707d
skinnypigeon <euan.blackledge@soprasteria.com>
2021-07-23 12:52:20 +0100

    need the blockchain up and running so I can check the headers etc

--- code/api/functions/lineage.py
+++ code/api/functions/lineage.py
@@ -2,17 +2,12 @@ import json
 import requests
 from datetime import datetime
 
-
-
-
 body = {'rule_id': 123, 'serums_id': 123, 'hospital_ids': ['ZMC', 'FCRB']}
-
 
 def create_record_on_blockchain(body, jwt):
     headers = {
-        "Authorization": "Bearer {}".format(jwt),
-        "Accept: application/json",
-        "Content-Type": "application/json"
+        'Authorization': 'Bearer {}'.format(jwt),
+        'Content-Type': 'application/json'
     }
 
     data = {
@@ -22,17 +17,57 @@ def create_record_on_blockchain(body, jwt):
         'date': str(datetime.today())
     }
 
-    url = "localhost:30001/api/proof"
+    url = 'http://localhost:3000/proof'
 
-    response = requests.request("POST", url, headers=headers, data=data)
-    if response.status_code == 200:
-        return {"status_code": response.status_code, "body": response.json()}
-    else:
-        return {"status_code": response.status_code, "body": response.text}
+    try:
+        response = requests.request('POST', url, headers=headers, data=data)
+        if response.status_code == 200:
+            return {'status_code': response.status_code, 'body': response.json()}
+        else:
+            return {'status_code': response.status_code, 'body': response.text}
+    except Exception as e:
+        return {'error': str(e)}
+
+proof_id = create_record_on_blockchain(body, 'jkasjdsajkas')['body']['proofId']
+print(proof_id)
 
 
+def add_update_to_blockchain(proof_id, jwt, body):
+    headers = {
+        'Authorization': 'Bearer {}'.format(jwt),
+        'Content-Type': 'application/json'
+    }
 
+    data = {
+        'type': body['type'],
+        'hospitalId': body['hospital_id'],
+        'status': body['status'],
+        'content': body['content'],
+        'date': str(datetime.today())
+    }
 
-    print(data)
+    url = 'http://localhost:3000/proof/{}'.format(proof_id)
 
-create_record_on_blockchain(body)
+    try:
+        response = requests.request('PATCH', url, headers=headers, data=data)
+        if response.status_code == 200:
+            return {'status_code': response.status_code, 'body': response.json()}
+        else:
+            return {'status_code': response.status_code, 'body': response.text}
+    except Exception as e:
+        return {'error': str(e)}
+
+body = {
+    'type': 'data_vault_created',
+    'hospital_id': 'zmc',
+    'status': 'success',
+    'content': {
+        'schema': '_abc92039'
+    },
+    'date': str(datetime.today())
+}
+
+print(body)
+
+response = add_update_to_blockchain('abc123', 'this_is_my_jwt', body)
+print(response)
