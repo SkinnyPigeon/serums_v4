38c1a02b6a783569cd82604f1598354b595425a6
skinnypigeon <euan.blackledge@soprasteria.com>
2021-07-23 14:59:43 +0100

    added the tags files and done some population to test things

---
+++ code/api/sources/tags/ustan.py
@@ -0,0 +1,166 @@
+ustan_wearable = {}
+
+ustan_diagnostic = {}
+
+ustan_medication = {}
+
+ustan_patient_details = {}
+
+ustan_patient_address = {}
+
+ustan_patient_appointment = {}
+
+ustan_operations = {}
+
+ustan_documents = {}
+
+ustan_healthcare_providers = {}
+
+ustan_drugs_and_alcohol = {}
+
+ustan_allergies = {}
+
+ustan_additional_information = {}
+
+ustan_treatments = {}
+
+ustan_personal = {}
+
+ustan_all_1 = {'tag': 'all', 'table': 'zmc.cycles', 'fields': ['chi',
+                                                               'regime_id',
+                                                               'intention_id',
+                                                               'cycle_id',
+                                                               'drug_names',
+                                                               'diagnosis',
+                                                               'init_appointment_date',
+                                                               'elapsed_days',
+                                                               'interval_days',
+                                                               'appointment_date',
+                                                               'intention',
+                                                               'regime',
+                                                               'cycle',
+                                                               'p_ps',
+                                                               'ps',
+                                                               'nausea',
+                                                               'vomiting',
+                                                               'diarrhoea',
+                                                               'constipation',
+                                                               'oralMucositis',
+                                                               'oesophagitis',
+                                                               'neurotoxicity',
+                                                               'handFoot',
+                                                               'skin',
+                                                               'hypersensitivity',
+                                                               'fatigue',
+                                                               'required_doses'
+                                                               ], 'key_lookup': {}}
+ustan_all_2 = {'tag': 'all', 'table': 'zmc.general', 'fields': ['chi',
+                                                                'incidence_date',
+                                                                'site_icd_10',
+                                                                'name',
+                                                                'date_of_birth',
+                                                                'first_seen_date',
+                                                                'site',
+                                                                'histology',
+                                                                'primary',
+                                                                'metastasis1',
+                                                                'metastasis2',
+                                                                'metastasis3',
+                                                                'smid',
+                                                                'smid1',
+                                                                'cong_heart_fail_flag',
+                                                                'con_tiss_disease_rheum_flag',
+                                                                'dementia_flag',
+                                                                'pulmonary_flag',
+                                                                'con_tiss_flag',
+                                                                'diabetes_flag',
+                                                                'para_hemiplegia_flag',
+                                                                'renal_flag',
+                                                                'liver_flag',
+                                                                'aids_hiv_flag',
+                                                                'cancer_flag',
+                                                                'charlson_score',
+                                                                'dob',
+                                                                'age',
+                                                                'simd',
+                                                                'simd1',
+                                                                'side',
+                                                                'gender',
+                                                                'age_at_diagnosis',
+                                                                'weight',
+                                                                'bmi',
+                                                                'height',
+                                                                'religion',
+                                                                'civil_st',
+                                                                'ref_hospital',
+                                                                'postcode_pre',
+                                                                'postcode_suf',
+                                                                'stage',
+                                                                'stage_detail',
+                                                                'tnm_t',
+                                                                'tnm_t_detail',
+                                                                'tnm_n',
+                                                                'tnm_n_detail',
+                                                                'tnm_m',
+                                                                'perf_stat',
+                                                                'smr01_flag',
+                                                                'postcode',
+                                                                'gp_name',
+                                                                'death_flag',
+                                                                'survival_days',
+                                                                'dat_death',
+                                                                'gp_id'
+                                                                ], 'key_lookup': {}}
+ustan_all_3 = {'tag': 'all', 'table': 'zmc.intentions', 'fields': ['chi',
+                                                                   'patient_id',
+                                                                   'intention_id',
+                                                                   'intention_seq',
+                                                                   'first_intention',
+                                                                   'regime_ratio',
+                                                                   'cycle_ratio',
+                                                                   'intention',
+                                                                   'first_regime',
+                                                                   'init_appointment_date',
+                                                                   'elapsed_days',
+                                                                   'appointment_date'
+                                                                   ], 'key_lookup': {}}
+ustan_all_4 = {'tag': 'all', 'table': 'zmc.regimes', 'fields': ['chi',
+                                                                'intention_id',
+                                                                'regime_id',
+                                                                'regime_seq',
+                                                                'regime_ratio',
+                                                                'cycle_ratio',
+                                                                'intention',
+                                                                'prev_regime',
+                                                                'first_regime',
+                                                                'regime',
+                                                                'interval_days',
+                                                                'elapsed_days',
+                                                                'init_appointment_date',
+                                                                'appointment_date'
+                                                                ], 'key_lookup': {}}
+ustan_all_5 = {'tag': 'all', 'table': 'zmc.patients', 'fields': ['chi',
+                                                                 'patient_id',
+                                                                 'first_intention',
+                                                                 'appointment_date'
+                                                                 ], 'key_lookup': {}}
+
+ustan_tags = [ustan_diagnostic,
+              ustan_medication,
+              ustan_patient_details,
+              ustan_patient_address,
+              ustan_patient_appointment,
+              ustan_operations,
+              ustan_documents,
+              ustan_healthcare_providers,
+              ustan_drugs_and_alcohol,
+              ustan_allergies,
+              ustan_additional_information,
+              ustan_treatments,
+              ustan_personal,
+              ustan_all_1,
+              ustan_all_2,
+              ustan_all_3,
+              ustan_all_4,
+              ustan_all_5
+              ]
---
+++ code/api/sources/tags/zmc.py
@@ -0,0 +1,71 @@
+zmc_wearable = {'tag': 'wearable', 'table': 'zmc.wearable', 'fields': [
+    'patnr', 'date', 'w_time', 'w_steps', 'w_cad', 'sst', 'sst_time', 'cyc_time', 'cyc_steps', 'cyc_cad'], 'key_lookup': {}}
+
+zmc_diagnostic = {}
+
+zmc_medication = {}
+
+zmc_patient_details = {}
+
+zmc_patient_address = {}
+
+zmc_patient_appointment = {}
+
+zmc_operations = {}
+
+zmc_documents = {}
+
+zmc_healthcare_providers = {}
+
+zmc_drugs_and_alcohol = {}
+
+zmc_allergies = {}
+
+zmc_additional_information = {}
+
+zmc_treatments = {}
+
+zmc_personal = {}
+
+zmc_all_1 = zmc_wearable
+zmc_all_2 = zmc_diagnostic
+zmc_all_3 = zmc_medication
+zmc_all_4 = zmc_patient_details
+zmc_all_5 = zmc_patient_address
+zmc_all_6 = zmc_patient_appointment
+zmc_all_7 = zmc_operations
+zmc_all_8 = zmc_documents
+zmc_all_9 = zmc_healthcare_providers
+zmc_all_10 = zmc_drugs_and_alcohol
+zmc_all_12 = zmc_allergies
+zmc_all_13 = zmc_additional_information
+zmc_all_14 = zmc_treatments
+zmc_all_15 = zmc_personal
+
+zmc_tags = [zmc_diagnostic,
+            zmc_medication,
+            zmc_patient_details,
+            zmc_patient_address,
+            zmc_patient_appointment,
+            zmc_operations,
+            zmc_documents,
+            zmc_healthcare_providers,
+            zmc_drugs_and_alcohol,
+            zmc_allergies,
+            zmc_additional_information,
+            zmc_treatments,
+            zmc_personal,
+            zmc_all_1,
+            zmc_all_2,
+            zmc_all_3,
+            zmc_all_4,
+            zmc_all_5,
+            zmc_all_6,
+            zmc_all_7,
+            zmc_all_8,
+            zmc_all_9,
+            zmc_all_10,
+            zmc_all_12,
+            zmc_all_13,
+            zmc_all_14,
+            zmc_all_15]
