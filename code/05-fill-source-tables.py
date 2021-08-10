5cc64c7ea87ecd4db945762aea18d1a2b439babe
skinnypigeon <euan.blackledge@soprasteria.com>
2021-08-10 12:47:41 +0100

    added in the ustan logins to the tables

--- code/05-fill-source-tables.py
+++ code/05-fill-source-tables.py
@@ -22,8 +22,8 @@ engine = create_engine("postgresql://postgres:{}@localhost:{}/source".format(PAS
 
 # Creating source tables
 
-# directories = ['fcrb', 'ustan', 'zmc', 'ustan_ml']
-directories = ['fcrb']
+directories = ['fcrb', 'ustan', 'zmc', 'ustan_ml']
+# directories = ['fcrb']
 
 fcrb_tables = ['hospital_doctors', 'serums_ids', 'diagnostic', 'episode', 'medical_specialty', 'medication', 'monitoring_params', 'order_entry', 'patient_address', 'patient', 'professional', 'vital_signs']
 ustan_tables = ['hospital_doctors', 'serums_ids', 'cycles', 'general', 'intentions', 'patients', 'regimes', 'smr01', 'smr06']
