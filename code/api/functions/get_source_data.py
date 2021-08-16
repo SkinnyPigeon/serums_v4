ecb78169f51be7ad83ffa49a2b80750eb0735d4d
skinnypigeon <euan.blackledge@soprasteria.com>
2021-08-16 16:16:11 +0100

    added docstrings to sphr

--- code/api/functions/get_source_data.py
+++ code/api/functions/get_source_data.py
@@ -35,6 +35,20 @@ if PORT == None:
 # Helper functions
 
 def hospital_picker(hospital):
+    """Returns a lowercased hospital id and tag definitions.
+    
+            Parameters:
+
+                hospital (str): The internal reference for the hospitals within the Serums system
+
+            Returns: 
+
+                hospital_id (str): The hospital id is used throughout during schema selection in the database\n
+                hospital_tags (list): A list of tag definitions. These are designed by the hospitals to subset their data in ways that the patients can intuitively understand when they create rules. These definitions show:\n
+                \t\t - The source table that holds the data
+                \t\t - The columns within the source table that are governed by the tag
+
+    """
     if hospital == 'FCRB':
         return 'fcrb', fcrb_tags
     elif hospital == 'USTAN':
@@ -44,6 +58,16 @@ def hospital_picker(hospital):
 
 
 def setup_connection(schema):
+    """Creates a connection to the database.
+
+            Parameters:
+                
+                schema (str): The schema for the use case partner's data within the database
+
+            Returns:
+
+                connection (dict): This contains most of the important elements of the connection allowing for many different types of operations within the database
+    """
     engine = create_engine('postgresql://postgres:{}@localhost:{}/source'.format(PASSWORD, PORT))
     metadata = MetaData(schema=schema, bind=engine)
     metadata.reflect(engine)
@@ -55,10 +79,31 @@ def setup_connection(schema):
 
 
 def object_as_dict(obj):
-    return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
+    """Returns an object as a dictionary by list comprehension
+    
+            Parameters:
+
+                obj (obj): The object to be converted into a dictionary
+
+            Returns:
+
+                dict (dict): A dictionary based on the object
+    """
+    return {column.key: getattr(obj, column.key) for column in inspect(obj).mapper.column_attrs}
 
 
 def select_tags(tags_list, request_tags):
+    """Selects the relevant tag definition(s) based on the tags field in the request body
+    
+            Parameters:
+
+                tags_list (list): The tag definitions as selected by hospital_picker()\n
+                request_tags (list): The list of tags that are to be used as selected by the request body
+
+            Returns:
+
+                selected_tags (list): A list of the tag definitions that is based on the tags field in the request body
+    """
     selected_tags = []
     for request_tag in request_tags:
         for tag_definition in tags_list:
@@ -70,7 +115,19 @@ def select_tags(tags_list, request_tags):
     return selected_tags
 
 
-def get_classes_by_name(schema, base):
+def get_classes(schema, base):
+    """A very useful helper function for selecting the all of the SQLAlchemy class definitions within the database
+    
+            Parameters:
+
+                schema (str): The schema within the database to search through\n
+                base (Base): The SQLAlchemy Base instance that contains the relevant metadata to enable the search
+
+            Returns:
+
+                tables (dict): A dictionary that uses the table names as the keys and SQLAlchemy table classes as the values
+
+    """
     tables = {}
     for class_name in base._decl_class_registry.values():
         if hasattr(class_name, '__table__') and class_name.__table__.fullname not in ['{schema}.serums_ids'.format(schema=schema), '{schema}.hospital_doctors'.format(schema=schema)]:
@@ -79,6 +136,17 @@ def get_classes_by_name(schema, base):
 
 
 def convert_dates_to_string(df):
+    """Converts dates to strings for ease of display in the front end
+        
+            Parameters:
+
+                df (DataFrame): The Pandas DataFrame to have its dates converted
+
+            Returns:
+
+                df (DataFrame): The Pandas DataFrame with dates now held as strings
+    
+    """
     for column in df:
         if df.dtypes[column] in ['datetime64[ns]', 'object']:
             df[column] = df[column].astype(str)
@@ -86,6 +154,17 @@ def convert_dates_to_string(df):
 
 
 def convert_decimal_to_float(df):
+    """Converts decimals to floats so they can be transmitted within the JSON response
+        
+            Parameters:
+
+                df (DataFrame): The Pandas DataFrame to have its decimals converted
+
+            Returns:
+
+                df (DataFrame): The Pandas DataFrame with decimals now held as floats
+    
+    """
     for column in df:
         if df.dtypes[column] in ['object']:
             try:
@@ -95,6 +174,17 @@ def convert_decimal_to_float(df):
     return df
 
 def convert_tuples_to_dict(row, fields):
+    """Converts tuples to dictionaries allowing dynamically selected rows from the database to be stored as JSON
+    
+            Parameters:
+
+                row (tuple): Data is returned from SQLAlchemy as a generator of tuples. This is a single row from within it\n
+                fields (list): The list of fields from the tags definition that is used as part of the query function
+
+            Returns:
+
+                row (dict): A dictionary version of the row that uses the fields as the keys and the tuple elements as the values
+    """
     row_dict = {}
     if len(row) == len(fields):
         for index, column in enumerate(fields):
@@ -106,6 +196,17 @@ def convert_tuples_to_dict(row, fields):
 
 
 def select_source_patient_id_name(body):
+    """Selects the correct column name for the patient id column. Different hospitals use different names for the patient id column within their systems. This allows the Serums API to retrive this value for use in querying multiple databases
+    
+            Parameters:
+                
+                body (dict): The request body passed from the frontend
+
+            Returns:
+
+                column_name (str): The correct name for the patient id column within a hospital's system
+    
+    """
     connection = setup_connection(body)
     metadata = connection['metadata']
     table_dict = dict.fromkeys(metadata.sorted_tables)
@@ -118,6 +219,20 @@ def select_source_patient_id_name(body):
 
 
 def select_source_patient_id_value(session, id_class, serums_id, key_name):
+    """Selects the patient id within the hospital's source system. Serums uses a generic id that can be used to link multiple hospitals' data, however, when searching a hospital's system we must use their internal patient id.
+    
+            Parameters:
+
+                session (Session): The SQLAlchemy session to run the query with\n
+                id_class (SQLAlchemy Table): The SQLAlchemy Table class to be queried\n
+                serums_id (int): The id used throughout the serums network linking a single patient across multiple hospitals\n
+                key_name (str): The name of the patient id column within a hospital's system
+
+            Returns:
+
+                patient_id (int): The native patient id within the hospital's system
+    
+    """
     result = session.query(id_class).filter_by(serums_id=serums_id).one()
     res = object_as_dict(result)
     return res[key_name]
@@ -127,6 +242,21 @@ def select_source_patient_id_value(session, id_class, serums_id, key_name):
 # Selecting tabular data:
 
 def select_tabular_patient_data(session, tables, tag_definition, patient_id, key_name):
+    """Selects the tabular data within from a hospital's source system
+    
+            Parameters:
+
+                session (Session): The SQLAlchemy session to run the query with\n
+                tables (dict): A dictionary that uses the table names as the keys and SQLAlchemy table classes as the values\n
+                tag_definition (dict): A tag definition that is based on the tags field in the request body\n
+                patient_id (int): The native patient id within the hospital's system\n
+                key_name (str): The name of the patient id column within a hospital's system
+
+            Returns:
+
+                smart_patient_health_record (DataFrame): A DataFrame containing the selected patient data
+
+    """
     data = []
     table_class = tables[tag_definition['source']]
     fields = tag_definition['fields']
@@ -147,6 +277,18 @@ def select_tabular_patient_data(session, tables, tag_definition, patient_id, key
 
 
 def select_image_patient_data(session, tables, tag_definition, patient_id, key_name):
+    """WORK IN PROGRESS. WILL BE USED TO SELECT IMAGE DATA.
+    
+            Parameters:
+
+                session (Session): The SQLAlchemy session to run the query with\n
+                tables (dict): A dictionary that uses the table names as the keys and SQLAlchemy table classes as the values\n
+                tag_definition (dict): A tag definition that is based on the tags field in the request body\n
+                patient_id (int): The native patient id within the hospital's system\n
+                key_name (str): The name of the patient id column within a hospital's system
+
+    
+    """
     data = []
     table_class = tables[tag_definition['source']]
     fields = tag_definition['fields']
@@ -170,9 +312,23 @@ def select_image_patient_data(session, tables, tag_definition, patient_id, key_n
 
 
 def select_patient_data(connection, tags_definitions, patient_id, key_name):
+    """Used to determine the type of data selection to be used i.e. image, tabular, or graph. Calls the relevant function.
+    
+            Parameters:
+
+                connection (dict): This contains most of the important elements of the connection allowing for many different types of operations within the database\n
+                tag_definitions (list): A list of tag definitions. These are designed by the hospitals to subset their data in ways that the patients can intuitively understand when they create rules. These definitions show:\n
+                patient_id (int): The native patient id within the hospital's system\n
+                key_name (str): The name of the patient id column within a hospital's system
+
+            Returns:
+
+                smart_patient_health_record (DataFrame): A DataFrame containing the selected patient data
+
+    """
     session = connection['session']
     results = {}
-    tables = get_classes_by_name(connection['schema'], connection['base'])
+    tables = get_classes(connection['schema'], connection['base'])
     for tag_definition in tags_definitions:
         if tag_definition['table']:
             results[tag_definition['source']] = select_tabular_patient_data(session, tables, tag_definition, patient_id, key_name)
@@ -182,6 +338,16 @@ def select_patient_data(connection, tags_definitions, patient_id, key_name):
 
 
 def get_patient_data(body):
+    """The main function for generating the Smart Patient Health Record
+    
+            Parameters:
+
+                body (dict): The request body from the api call
+
+            Returns:
+
+                smart_patient_health_record (DataFrame): A DataFrame containing the selected patient data
+    """
     results = {}
     for hospital_id in body['hospital_ids']:
         try:
@@ -202,4 +368,3 @@ def get_patient_data(body):
             if str(e) == "No row was found for one()":
                 results[hospital_id] = {"Error": "Serums ID not found with healthcare provider: {}".format(hospital_id)}
     return results
-
