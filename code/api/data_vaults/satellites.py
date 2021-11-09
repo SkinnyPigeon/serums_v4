e03395620f9dd5cb9968bb5289b75de7e8d80da4
skinnypigeon <euan.blackledge@soprasteria.com>
2021-11-09 17:36:30 +0100

    parsing the dates and stripping the stirngs

--- code/api/data_vaults/satellites.py
+++ code/api/data_vaults/satellites.py
@@ -1,6 +1,7 @@
 from sources.keys_and_sats.zmc_keys_and_sats import zmc_sats, zmc_keys
 from sources.keys_and_sats.fcrb_keys_and_sats import fcrb_sats, fcrb_keys
 from sources.keys_and_sats.ustan_keys_and_sats import ustan_sats, ustan_keys
+import datetime
 
 def hospital_picker(hospital):
     if hospital == 'ZMC':
@@ -9,6 +10,13 @@ def hospital_picker(hospital):
         return fcrb_sats, fcrb_keys
     elif hospital == 'USTAN':
         return ustan_sats, ustan_keys
+
+def process_value(value):
+    if isinstance(value, datetime.datetime):
+        return value.strftime("%d/%m/%Y %H:%M:%S")
+    if isinstance(value, str):
+        return value.strip()
+    return value
 
 def process_satellites(data):
     results = {}
@@ -21,14 +29,10 @@ def process_satellites(data):
             results[hospital][table_name]['links'] = satellite_definitions[table_name]['links']
             for satellite_name in satellite_definitions[table_name]:
                 if satellite_name != 'links':
-                    print(satellite_name)
-                    if satellite_name == 'sat_time_complaints_and_diagnosis':
-                        for row in source_data:
-                            print(row)
                     columns = satellite_definitions[table_name][satellite_name]['columns']
                     results[hospital][table_name][satellite_name] = {}
                     results[hospital][table_name][satellite_name]['hub'] = satellite_definitions[table_name][satellite_name]['hub']
-                    results[hospital][table_name][satellite_name]['data'] = [{k: row[k] for k in row if k in columns} for row in source_data]
+                    results[hospital][table_name][satellite_name]['data'] = [{k: process_value(row[k]) for k in row if k in columns} for row in source_data]
                     results[hospital][table_name][satellite_name]['keys'] = [{k: row[k] for k in row if k in keys} for row in source_data]
     return results
 
