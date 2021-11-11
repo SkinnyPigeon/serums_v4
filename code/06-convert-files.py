91f0f1deb60837464cf0fd2aa4fd21f98278b102
skinnypigeon <euan.blackledge@soprasteria.com>
2021-11-11 15:20:12 +0100

    changing the way the documents and images are added

--- code/06-convert-files.py
+++ code/06-convert-files.py
@@ -74,11 +74,39 @@ with open('{document_path}/converted/concat.png'.format(document_path=document_p
 
 # Saving to database
 
-#THIS WOULD NEED TO BE A LIST OF ALL THE AVAILABLE PATNRS. DOABLE BUT WILL TAKE A LONG TIME
-images = {'patnr': [1075835, 1075835], 'image_title': ['Right hip x-ray', 'Right pelvis x-ray'], 'type': ['x-ray', 'x-ray'], 'date': ['2020-09-01', '2020-09-01'], 'image': [image_1, image_2]}
-document = {'patnr': [1075835, 1075835], 'document_title': ['Resultaten verwijzing orthopedisch onderzoek', 'Operatieraport vervanging rechter heupgewricht'], 'type': ['orthopedics', 'orthopedics'], 'date': ['2020.02.14', '2020.03.16'],'document': [document1, document1]}
+patient_details = pd.read_csv(f"{project_folder}/code/api/reset_sql/data/zmc/patient_details.csv")
+
+patnr_list = list(set(patient_details['patnr']))
+patnr_count = len(patnr_list)
+full_patnr_list = patnr_list + patnr_list
+
+rhx = ['Right hip x-ray' for i in range(patnr_count)]
+rpx = ['Right pelvis x-ray' for i in range(patnr_count)]
+x_ray_list = rhx + rpx
+x_types = ['x-ray' for i in range(2 * patnr_count)]
+x_ray_dates = ['2020-09-01' for i in range(2 * patnr_count)]
+rhi = [image_1 for i in range(patnr_count)]
+rpi = [image_2 for i in range(patnr_count)]
+x_ray_images = rhi + rpi
+
+dt1 = ['Resultaten verwijzing orthopedisch onderzoek' for i in range(patnr_count)]
+dt2 = ['Operatieraport vervanging rechter heupgewricht' for i in range(patnr_count)]
+document_list = dt1 + dt2
+d_types = ['orthopedics' for i in range(2 * patnr_count)]
+dd1 = ['2020.02.14' for i in range(patnr_count)]
+dd2 = ['2020.03.16' for i in range(patnr_count)]
+document_dates = dd1 + dd2
+doc1 = [document1 for i in range(patnr_count)]
+doc2 = [document2 for i in range(patnr_count)]
+document_list = doc1 + doc2
+
+images = {'patnr': full_patnr_list, 'image_title': x_ray_list, 'type': x_types, 'date': x_ray_dates, 'image': x_ray_images}
+documents = {'patnr': full_patnr_list, 'document_title': document_list, 'type': d_types, 'date': document_dates,'document': document_list}
+
+# images = {'patnr': [1075835, 1075835], 'image_title': ['Right hip x-ray', 'Right pelvis x-ray'], 'type': ['x-ray', 'x-ray'], 'date': ['2020-09-01', '2020-09-01'], 'image': [image_1, image_2]}
+# document = {'patnr': [1075835, 1075835], 'document_title': ['Resultaten verwijzing orthopedisch onderzoek', 'Operatieraport vervanging rechter heupgewricht'], 'type': ['orthopedics', 'orthopedics'], 'date': ['2020.02.14', '2020.03.16'],'document': [document1, document1]}
 image_df = pd.DataFrame.from_dict(images)
-document_df = pd.DataFrame.from_dict(document)
+document_df = pd.DataFrame.from_dict(documents)
 
 image_df.to_sql('images', con=engine, if_exists='append', index=False, schema='zmc')
 document_df.to_sql('documents', con=engine, if_exists='append', index=False, schema='zmc')
